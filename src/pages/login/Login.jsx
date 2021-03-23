89cd9808cfb2f909f69b6761d97693549b9332e8
Dmitry Grinchenko <ra1d3r@i.ua>
2021-03-23 23:00:27 +0200

    added controled form, fixed styles

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -2,15 +2,46 @@ import React, { Component } from 'react'
 
 import styles from "./Login.module.css"
 
+const InitialState = {
+    email: '',
+    password:''
+}
 export default class Login extends Component {
+
+    state = {
+        ...InitialState
+    }
+    handleChange = (event) => {
+        const { name, value } = event.target
+        this.setState({[name]:value})
+    }
+
+
     render() {
+        const {email, password}=this.state
         return (
             <form className={styles.form}>
                 <label htmlFor="email" className={styles.label}>Email
-                    <input id="email" name='email' type="mail" className={styles.input} placeholder="Email*" required />
+                    <input id="email"
+                        name='email'
+                        type="mail"
+                        className={styles.input}
+                        placeholder="Email*"
+                        value={email}
+                        onChange={this.handleChange}
+                        required
+                    />
                 </label>
                 <label htmlFor="password"  className={styles.label}>Password
-                    <input id="password" name='password' type="password" className={styles.input} placeholder="Password*" required />
+                    <input id="password"
+                        name='password'
+                        type="password"
+                        className={styles.input}
+                        placeholder="Password*"
+                        value={password}
+                        onChange={this.handleChange}
+                        required
+                    />
                 </label>
                 <button type="submit"  className={styles.submitBtn}>Log in</button>
             </form>
--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,18 +1,60 @@
 import React, { Component } from 'react'
 import styles from "./Register.module.css"
 
+
+const InitialState = {
+    name:'',
+    email: '',
+    password:''
+}
 export default class Register extends Component {
+
+     state = {
+        ...InitialState
+    }
+
+ handleChange = (event) => {
+        const { name, value } = event.target
+        this.setState({[name]:value})
+    }
+
     render() {
+const {name, email, password} = this.state
+
         return (
             <form className={styles.form}>
                 <label htmlFor="name"  className={styles.label}> Name
-                    <input id="name" name='email' type="text" className={styles.input} placeholder="Name*" required />
+                    <input id="name"
+                        name='name'
+                        type="text"
+                        className={styles.input}
+                        placeholder="Name*"
+                        value={name}
+                        onChange={this.handleChange}
+                        required
+                    />
                 </label>
                 <label htmlFor="email"  className={styles.label}>Email
-                    <input id="email" name='email' type="mail" className={styles.input} placeholder="Email*" required />
+                    <input id="email"
+                        name='email'
+                        type="mail"
+                        className={styles.input}
+                        placeholder="Email*"
+                        value={email}
+                        onChange={this.handleChange}
+                        required
+                    />
                 </label>
                 <label htmlFor="password"  className={styles.label}>Password
-                    <input id="password" name='password' type="password" className={styles.input} placeholder="Password*" required />
+                    <input id="password"
+                        name='password'
+                        type="password"
+                        className={styles.input}
+                        placeholder="Password*"
+                        value={password}
+                        onChange={this.handleChange}
+                        required
+                    />
                 </label>
                 <button type="submit"  className={styles.submitBtn}>Sign up</button>
             </form>
